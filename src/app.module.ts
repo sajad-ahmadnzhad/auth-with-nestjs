8fc380d5c822464a60a0f1f9f3248176fcbbae74
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-05-14 15:43:57 +0430

    feat: config app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { CacheModule } from "@nestjs/cache-manager";
+import { redisStore } from "cache-manager-redis-yet";
+import { APP_PIPE } from "@nestjs/core";
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    MongooseModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>("MONGODB_URI"),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env.development" }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      async useFactory(config: ConfigService) {
+        const store = await redisStore({
+          socket: {
+            host: config.get<string>("REDIS_HOST"),
+          },
+        });
+
+        return { store };
+      },
+    }),
+  ],
+  providers: [
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+  ],
 })
 export class AppModule {}
