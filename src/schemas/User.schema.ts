bac39c868a17b8ca2ad8e64aea8723c09dbb3016
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-05-14 15:52:39 +0430

    feat: create resource auth and user schema

---
+++ src/schemas/User.schema.ts
@@ -0,0 +1,30 @@
+import { Prop } from "@nestjs/mongoose";
+
+export class User {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  username: string;
+
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({
+    type: String,
+    default: "/public/uploads/usersAvatar/custom-avatar.jpg",
+  })
+  avatarURL: string;
+
+  @Prop({ type: Boolean, default: false })
+  isAdmin: string;
+
+  @Prop({ type: Boolean, default: false })
+  isSuperAdmin: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  isVerifyEmail: boolean;
+}
